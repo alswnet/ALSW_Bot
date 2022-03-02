eded56713112f5b0bbb11ee4199ee4a9e4358704
chepecarlos <chepecarlos@alswblog.org>
2022-03-02 15:18:57 -0600

    Salvar por defecto

--- src/alswbot/alswbot.py
+++ src/alswbot/alswbot.py
@@ -1,4 +1,3 @@
-
 import argparse
 
 import MiLibrerias
@@ -7,25 +6,30 @@ from bot.youtubebot import ChatYoutube
 
 logger = MiLibrerias.ConfigurarLogging(__name__)
 
-def ArgumentosCLI():
-    parser = argparse.ArgumentParser(prog="alswbot", description='Bot para chat de ALSW')
-    parser.add_argument("--twitch", '-t', help="Activar Bot Twitch", action="store_true")
-    parser.add_argument("--youtube", '-y', help="Activar Bot Youtube")
 
-    parser.add_argument("--salvar", '-s', help="Salvar Chat en Archivo", action="store_true")
+def ArgumentosCLI():
+    parser = argparse.ArgumentParser(prog="alswbot", description="Bot para chat de ALSW")
+    parser.add_argument("--twitch", "-t", help="Activar Bot Twitch", action="store_true")
+    parser.add_argument("--youtube", "-y", help="Activar Bot Youtube")
+
+    parser.add_argument("--noSalvar", "-no_s", help="No Salvar Chat en Archivo", action="store_true")
     return parser.parse_args()
-    
+
 
 def main():
     logger.info("Iniciando el bot")
     args = ArgumentosCLI()
+    salvar = True
+    if args.noSalvar is None or args.noSalvar:
+        salvar = False
+
     if args.youtube:
         print(f"valor de youtube {args.youtube}")
-        ChatYoutube(args.youtube, args.salvar)
+        ChatYoutube(args.youtube, salvar)
     if args.twitch:
         bot = twithbot()
         bot.conectar()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
